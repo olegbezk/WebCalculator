90b2aea7a4316ee1f6d11995f81ce3b9774e6c87
oleg.bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2016-06-17 01:42:30 +0300

    flyway

--- src/main/java/com/web/calculator/App.java
+++ src/main/java/com/web/calculator/App.java
@@ -1,11 +1,14 @@
 package com.web.calculator;
 
+import org.flywaydb.core.Flyway;
+
 /**
  * Created by oleg.bezkorovaynyi on 17 Jun 2016.
  */
 
 public class App {
     public static void main(String[] args) {
-
+        Flyway flyway = new Flyway();
+        flyway.migrate();
     }
 }
