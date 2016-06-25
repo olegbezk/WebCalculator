465bf4ac0d174bc0b8d773ca804a59f18be53c31
oleg.bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2016-06-26 01:23:26 +0300

    implementation for UI

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,11 +2,60 @@ var calculatorApp = angular.module('calculatorApp', ['calculatorModule']);
 var calculatorModule = angular.module('calculatorModule', []);
 
 calculatorModule.controller('calculatorController', function($scope, $http) {
-$http.get('http://localhost:8080/transaction-log/logs/')
-       .then(function(res){
-          $scope.results = res.data;
+
+    var singleOp;
+
+    function showHistory() {
+        $http.get('http://localhost:8080/transaction-log/logs/')
+            .then(function(res){
+            $scope.results = res.data;
+            });
+    }
+
+    function postData() {
+
+        substituteOperation();
+
+        var calcData = {
+            leftOperand: calculatorModel.firstNumber,
+            rightOperand: calculatorModel.secondNumber,
+            arithmeticOperation: calculatorModel.subOperation
+            };
+        $http.post(
+            'http://localhost:8080/transaction-log/add/',
+            JSON.stringify(calcData),
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).success(function (data) {
+            $scope.calcResult = data;
         });
+    }
+
+    function substituteOperation() {
+        switch(calculatorModel.operation) {
+            case "+":
+                calculatorModel.subOperation = "ADD";
+                break;
+            case "-":
+                calculatorModel.subOperation = "SUBTRACT";
+                break;
+            case "*":
+                calculatorModel.subOperation = "MULTIPLY";
+                break;
+            case "/":
+                calculatorModel.subOperation = "DIVIDE";
+                break;
+            case "^":
+                calculatorModel.subOperation = "POWER";
+                break;
+        }
+    }
+
     $scope.calculator = calculatorModel;
+
     $scope.numberButtonClicked = function(clickedNumber) {
         if(calculatorModel.currentNumber === "0") {
             calculatorModel.currentNumber = "";
@@ -14,6 +63,13 @@ $http.get('http://localhost:8080/transaction-log/logs/')
         }
 
         calculatorModel.currentNumber += clickedNumber;
+
+        if(calculatorModel.operation == "") {
+            calculatorModel.firstNumber = parseFloat(clickedNumber);
+        } else {
+            calculatorModel.secondNumber = parseFloat(clickedNumber);
+            singleOp = 0;
+        }
         calculatorModel.currentDisplay += clickedNumber;
     };
 
@@ -22,79 +78,69 @@ $http.get('http://localhost:8080/transaction-log/logs/')
     };
 
     $scope.enterClicked = function() {
-        calculatorModel.calculate();
-        calculatorModel.currentDisplay = calculatorModel.result;
+        if(singleOp == 0) {
+            postData();
+            calculatorModel.calculate();
+            calculatorModel.currentDisplay = calculatorModel.result;
+            showHistory();
+            singleOp++;
+        }
     };
 
     $scope.resetClicked = function() {
-         calculatorModel.reset();
+        calculatorModel.reset();
     };
 });
 
-		calculatorModel = {
-			result: 0, // Holds the actual result in memory
-			operation: "",
-			currentNumber: "0",
-			currentDisplay: "", // Shows the input until the result is shown
+calculatorModel = {
+    result: 0,
+    operation: "",
+    subOperation: "",
+    currentNumber: "0",
+    currentDisplay: "",
+    firstNumber: 0,
+    secondNumber: 0,
 
-			reset: function() {
-				this.result = 0;
-				this.operation = "";
-				this.currentNumber = "0";
-				this.currentDisplay = "" ;
-			},
+    reset: function() {
+        this.result = 0;
+        this.operation = "";
+        this.currentNumber = "0";
+        this.currentDisplay = "" ;
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+    },
 
-			setOperation: function(operationToSet) {
-				this.operation = operationToSet;
-				if(calculatorModel.currentNumber === "0") {
-					this.currentDisplay += "0";
-				}
+    setOperation: function(operationToSet) {
+        this.operation = operationToSet;
+        if(calculatorModel.currentNumber === "0") {
+            this.currentDisplay += "0";
+        }
 
-				this.currentDisplay += " " + this.operation + " ";
-				this.calculate();
+        this.currentDisplay += " " + this.operation + " ";
+        //this.calculate();
 
-				this.currentNumber = "";
-			},
+        this.currentNumber = "";
+    },
 
-			substituteSign: function(operationSign) {
-			    switch(operationSign) {
-			    case ADD:
-			        this.operation = "+";
-			        break;
-			    case DIVIDE:
-                    this.operation = "/";
-                     break;
-                case MULTIPLY:
-                     this.operation = "*";
-                     break;
-                case SUBTRACT:
-                     this.operation = "-";
-                     break;
-                case POWER:
-                     this.operation = "^";
-                     break;
-			    }
-			},
+    calculate: function() {
 
-			calculate: function() {
+        switch(this.operation) {
+            case "+":
+                this.result = this.result + parseFloat(this.currentNumber);
+                break;
 
-				switch(this.operation) {
-					case "+":
-						this.result = this.result + parseFloat(this.currentNumber);
-						break;
+            case "-":
+                this.result = this.result - parseFloat(this.currentNumber);
+                break;
 
-					case "-":
-						this.result = this.result - parseFloat(this.currentNumber);
-						break;
+            case "*":
+                this.result = this.result * parseFloat(this.currentNumber);
+                break;
 
-				    case "*":
-				        this.result = this.result * parseFloat(this.currentNumber);
-				        break;
+            case "/":
+                this.result = this.result / parseFloat(this.currentNumber);
+                break;
+        }
+    }
 
-				    case "/":
-                        this.result = this.result / parseFloat(this.currentNumber);
-                    	break;
-				}
-			}
-
-		};
+};
